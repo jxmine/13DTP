ca5ada7261574e3d206700166ac3e5ceedb9f45a
jxmine <jas9967765@gmail.com>
2021-02-16 11:44:53 +1300

    try accept

--- programs/myownbackpack.py
+++ programs/myownbackpack.py
@@ -1,10 +1,15 @@
 def mainmenu():
     while True:
-        choice = input("""
-        What do you want to do today?
-        1. View backpack list
-        2. Add an item to the backpack
-        3. Delete an item from the backpack
-        4. Check the amount of items in the backpack 
-        5. Clear the backpack
-        """)
+        try:
+            choice = int(input("""
+What do you want to do today?
+1. View backpack list
+2. Add an item to the backpack
+3. Delete an item from the backpack
+4. Check the amount of items in the backpack 
+5. Clear the backpack
+"""))
+            break
+        except:
+            print("choose a valid option!")
+
