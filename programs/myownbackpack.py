7b9c0ae04f5fc05339154788f466dd51c422770b
jxmine <jas9967765@gmail.com>
2021-02-16 11:37:29 +1300

    main menu

---
+++ programs/myownbackpack.py
@@ -0,0 +1,10 @@
+def mainmenu():
+    while True:
+        choice = input("""
+        What do you want to do today?
+        1. View backpack list
+        2. Add an item to the backpack
+        3. Delete an item from the backpack
+        4. Check the amount of items in the backpack 
+        5. Clear the backpack
+        """)
